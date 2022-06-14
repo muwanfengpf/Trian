5f50df9bca01843c1a26680b78b15c5ee07c904e
wuyu <2982313745@qq.com>
2022-06-14 17:17:36 +0800

    master commit2

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -4,5 +4,7 @@ public class git {
         System.out.println("nizaiganshenm2");
         System.out.println("nizaiganshenm3");
         System.out.println("nizaiganshenm4");
+        System.out.println("nizaiganshenm6 master");
     }
+
 }
