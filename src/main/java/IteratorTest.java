e3717f84b7cddb3eaac02163518b9928e9849cbf
wuyu <2982313745@qq.com>
2022-06-14 16:57:48 +0800

    first commit

---
+++ src/main/java/IteratorTest.java
@@ -0,0 +1,14 @@
+
+import com.sun.xml.internal.ws.server.sei.SEIInvokerTube;
+import sun.java2d.pipe.SpanIterator;
+
+import java.security.PublicKey;
+import java.util.*;
+import java.util.TreeMap;
+public class IteratorTest {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/person.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class person  {
+
+    private Integer id;
+    private String name;
+
+}
