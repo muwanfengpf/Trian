c5320c17e10d69d533da834b34092b0d95b51d41
wuyu <2982313745@qq.com>
2022-06-14 17:01:06 +0800

    third commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,5 +2,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("nizaiganshenm");
         System.out.println("nizaiganshenm2");
+        System.out.println("nizaiganshenm3");
     }
 }
